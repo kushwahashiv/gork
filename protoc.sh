2682f06c5d0a6c9d353ccdcc66b7cdbd3f55df78
Ivan Koptiev <ikoptev@gmail.com>
2017-06-24 20:11:31 +0300

    remove in favour of Makefile

--- protoc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-protoc \
-    --proto_path=${GOPATH}/src/ \
-    --proto_path ./transformers/gateways/grpc/proto/protobuf/ \
-    --proto_path=./vendor/github.com/mwitkow/go-proto-validators/ \
-    --gofast_out=plugins=grpc:./transformers/gateways/grpc/proto/ \
-    --govalidators_out=gogoimport=true:./transformers/gateways/grpc/proto/ \
-    ./transformers/gateways/grpc/proto/protobuf/*.proto
